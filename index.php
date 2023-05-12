38f7c5410687b6d1994c3c3ce13d81262b3c6805
2140467 <133364147+2140467@users.noreply.github.com>
2023-05-12 14:46:23 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Document</title>
+</head>
+<body>
+	<h1>welkom op deze pagina </h1>
+	<?php echo "Hello world"; ?>
+
+	<br>
+	<?php
+	$var2 = 10;
+	$var1 = 12;
+	$sum = $var1 + $var2;
+	?>
+
+	<br>
+	<?php
+	$var2 = 10;
+	$var2 = 12;
+	$sum = 10*12;
+	?>
+
+	<br>
+	<?php
+	$naam="Moustafa";
+	echo"welkom" .$naam;
+	?>
+
+<br>
+<?php
+
+// deze code bevat veel fouten haal alle fouten eruit!
+//
+
+$mijnNaam="Carl";
+$jouwNaam="Camel";
+echo $mijnNaam.  " zit bij " . $jouwNaam . " in de klas!"
+
+?>
+	
+	
+</body>
+</html>

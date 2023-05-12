72013fcc8682362cabcbccf28c5cdfd38ad7a7a0
2140467 <133364147+2140467@users.noreply.github.com>
2023-05-12 15:43:39 +0200

    Add files via upload

---
+++ get.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=\, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+<form method="GET" action="verwerking.php">
+
+<input type="text" name="name" placeholder="Naam" required> <br>
+
+<input type="text" name="achtername" placeholder="achterNaam" required> <br>
+
+<input type="text" name="leeftijd" placeholder="leeftijd" required> <br>
+
+<input type="email" name="email" placeholder="Email" required> <br>
+
+<input type="text" name="adres" placeholder="adres" required> <br>
+
+<input type="submit" value="Verstuur">
+
+</form>
+    
+</body>
+</html>
